7f9e502f0adb67fa56816335a89c4521d1c05a92
370338 <beedleowen@gmail.com>
2024-08-23 22:46:03 -0700

    changed base back to gyhuji

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/gyhuji/",
+  base: "/gyhuji/",
   plugins: [react()],
   /*server: {
     hmr: {
